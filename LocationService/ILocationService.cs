97a5ce284933b5d138d0df90c3f65d76d010ed09
razy <razylo@hotmail.com>
2014-09-19 15:56:10 +1000

    Web service

---
+++ LocationService/ILocationService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace LocationService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface ILocationService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "/GetLocation")]
+        string GetLocation ( Location location);
+        //string ReturnAlertInfo ( string ID );
+        [OperationContract]
+        [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "/Alert")]
+        string Alert (Notification notification);
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class Location
+    {
+        [DataMember]
+        public float Latitude
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public float Longitude
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public string Patient_ID
+        {
+            get;
+            set;
+        }
+    }
+
+    [DataContract]
+    public class Notification
+    {
+        [DataMember]
+        public string Patient_ID
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string Message
+        {
+            get;
+            set;
+        }
+    }
+    
+}
---
+++ LocationService/LocationService.svc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace LocationService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class LocationService : ILocationService
+    {
+        BusinessTier.CareTakerOperation carer = new BusinessTier.CareTakerOperation();
+        BusinessTier.SendNotification sender = new BusinessTier.SendNotification();
+        public string GetLocation ( Location location )
+        {
+            carer.InsertLocationRecord(location.Patient_ID, location.Latitude, location.Longitude);
+            return "Success...Please Wait";
+        }
+        /*
+        public string ReturnAlertInfo ( string ID )
+        {
+            return "Patient " + ID + " is out of fence.......!";
+        }
+         * */
+        public string Alert ( Notification notice)
+        {
+            if ( carer.InsertNotification(notice.Patient_ID, notice.Message) == true )
+            {
+                sender.SendNotificationToCarer(notice.Patient_ID, notice.Message);
+                return "Success...Please Wait";
+            }
+            else
+            {
+                return "Failed...Please Contact Admin";
+            }
+        }
+    }
+}
